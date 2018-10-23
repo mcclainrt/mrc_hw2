83b58e98236479973eefdac2274178bf9e64d295
mcclainrt <mcclainrt@gmail.com>
2018-10-23 10:49:09 -0700

    Trutleletter script

---
+++ catkin_ws/src/mrc_hw2/turtleletter.sh
@@ -0,0 +1,6 @@
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0, 0, 0]' '[0, 0, 1.56]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[3, 0, 0]' '[0, 0, 0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4, 0, 0]' '[0, 0, -4.712]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[.8, 0, 0]' '[0, 0, 0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0, 0, 0]' '[0, 0, 2.2]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[2.7, 0, 0]' '[0, 0, 0]'
